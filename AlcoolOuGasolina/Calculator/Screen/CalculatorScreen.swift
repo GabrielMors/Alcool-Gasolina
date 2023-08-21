dcfae86c953c078eff07d5ae65811fa56865c00e
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-21 11:25:51 -0400

    criei a imagem de fundo da calculator

--- AlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
+++ AlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
@@ -9,5 +9,35 @@ import UIKit
 
 class CalculatorScreen: UIView {
     
-
+    lazy var backgroundImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "BG BLUR DARK")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    private func addSubViews() {
+        addSubview(backgroundImageView)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        
+        ])
+    }
 }

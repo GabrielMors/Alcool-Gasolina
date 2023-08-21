2a4e9aa5546b3798aca5aaa7c6c73902ab4e550d
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-21 17:11:08 -0400

    transitando dados entre as telas

--- AlcoolOuGasolina/Calculator/CalculatorViewController.swift
+++ AlcoolOuGasolina/Calculator/CalculatorViewController.swift
@@ -52,24 +52,23 @@ extension CalculatorViewController: CalculatorScreenProtocol {
     
     func tappedCalculateButton() {
         
-        let viewController = ResultViewController()
-        navigationController?.pushViewController(viewController, animated: true)
-        
-//        if validateTextField() {
-//
-//            let formatter = NumberFormatter()
-//            formatter.numberStyle = .decimal
-//
-//            let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
-//            let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
-//
-//            let viewController = ResultViewController()
-//            if ethanolPrice / gasPrice > 0.7 {
-//                print("Melhor usar Gasolina!")
-//            } else {
-//                print("Melhor usar Álcool!")
-//            }
-//            navigationController?.pushViewController(viewController, animated: true)
-//        }
+        if validateTextField() {
+
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+
+            let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+            let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+
+            var viewController: ResultViewController?
+            if ethanolPrice / gasPrice > 0.7 {
+                print("Melhor usar Gasolina!")
+                viewController = ResultViewController(bestFuel: .gas)
+            } else {
+                print("Melhor usar Álcool!")
+                viewController = ResultViewController(bestFuel: .ethanol)
+            }
+            navigationController?.pushViewController(viewController ?? UIViewController(), animated: true)
+        }
     }
 }

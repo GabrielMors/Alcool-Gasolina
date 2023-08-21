dee7e5c0ef701d7d17d0771f930c07b789fec8fc
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-21 13:19:25 -0400

    criando logica para calcular o preco do conbustivel

--- AlcoolOuGasolina/Calculator/CalculatorViewController.swift
+++ AlcoolOuGasolina/Calculator/CalculatorViewController.swift
@@ -30,6 +30,18 @@ extension CalculatorViewController: CalculatorScreenProtocol {
     }
     
     func tappedCalculateButton() {
-        print(#function)
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+        let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.0") as? Double) ?? 0.0
+        
+        
+        if ethanolPrice / gasPrice > 0.7 {
+            print("Melhor usar Gasolina!")
+        } else {
+            print("Melhor usar Álcool!")
+        }
     }
 }

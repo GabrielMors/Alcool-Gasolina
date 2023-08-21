f5a0c7a8f5c7a1cb8761afe2b99d4c77b1fd7999
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-21 12:50:02 -0400

    criado botao de calcular e botao de voltar

--- AlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
+++ AlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
@@ -17,6 +17,20 @@ class CalculatorScreen: UIView {
         return image
     }()
     
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Botão Back", for: .normal)
+//        button.clipsToBounds = true
+//        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(tappedBackButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func tappedBackButton() {
+        
+    }
+    
     lazy var logoAppImageView: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -53,11 +67,30 @@ class CalculatorScreen: UIView {
         return textField
     }()
     
+    lazy var calculateButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Calcular", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1.0)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(tappedCalculateButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func tappedCalculateButton() {
+        
+    }
+    
     private func addSubViews() {
         addSubview(backgroundImageView)
+        addSubview(backButton)
         addSubview(logoAppImageView)
         addSubview(ethanolPriceTextField)
         addSubview(gasPriceTextField)
+        addSubview(calculateButton)
     }
     
     override init(frame: CGRect) {
@@ -77,11 +110,15 @@ class CalculatorScreen: UIView {
             backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
         
-            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 60),
+            backButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            
+            
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 150),
             logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             
-            ethanolPriceTextField.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 200),
+            ethanolPriceTextField.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 134),
             ethanolPriceTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             ethanolPriceTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             ethanolPriceTextField.heightAnchor.constraint(equalToConstant: 45),
@@ -90,6 +127,11 @@ class CalculatorScreen: UIView {
             gasPriceTextField.leadingAnchor.constraint(equalTo: ethanolPriceTextField.leadingAnchor),
             gasPriceTextField.trailingAnchor.constraint(equalTo: ethanolPriceTextField.trailingAnchor),
             gasPriceTextField.heightAnchor.constraint(equalTo: ethanolPriceTextField.heightAnchor),
+            
+            calculateButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -135),
+            calculateButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            calculateButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            calculateButton.heightAnchor.constraint(equalToConstant: 45),
             
             
         ])
